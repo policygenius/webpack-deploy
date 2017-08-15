d6dc4483528283fe2c192169c3744323f2dc024c
Patrick Kim <patrick.kim@policygenius.com>
2017-08-15 11:35:41 -0400

    WIP webpack-gcloud

--- tasks/deploy-gcloud.js
+++ tasks/deploy-gcloud.js
@@ -10,7 +10,6 @@ const { getConfigFor } = require('./utils.js');
  * Deploy compiled, minified and fingerprinted assets to a Google Cloud Storage bucket.
  */
 function deployGcloud(config) {
-  console.log(config)
   if (!config.credentials.projectId || !config.credentials.keyFilename) {
     gutil.log(
       gutil.colors.red('Google Cloud credentials missing, skipping upload...'),
@@ -24,28 +23,34 @@ function deployGcloud(config) {
     keyFilename: config.credentials.keyFilename,
   });
 
-  const bucketName = config.params.Bucket;
+  const bucketName = config.credentials.params.Bucket;
   const bucket = gcs.bucket(bucketName);
 
-  gutil.log(`${gutil.colors.magenta(`gs://${bucketName} => uploading`)}`);
+  gutil.log(`${gutil.colors.magenta(`gs://${bucketName} => uploading`)} from ${config.assetsPath}`);
 
   return (
     gulp
       .src(config.assetsPath)
       .pipe(
         rename(function(filepath) {
+          gutil.log(`${gutil.colors.yellow(`org`)}`, filepath);
+
           filepath.dirname = path.join(config.dirname, filepath.dirname);
+
+          gutil.log(`${gutil.colors.yellow(`new`)}`, filepath);
         }),
       )
       .pipe(function(filepath) {
-        bucket.upload(
-          filepath,
-          {
-            destination: `${filepath}`,
-            public: true,
-            gzip: true,
-          },
-        )
+        gutil.log(`${gutil.colors.yellow(`upload`)}`, filepath);
+        // bucket.upload(
+        //   filepath,
+        //   {
+        //     destination: `${filepath}`,
+        //     public: true,
+        //     gzip: true,
+        //   },
+        // )
+        // .then(() => gutil.log(`${gutil.colors.dim('↑ Uploaded')} ${filepath}`));
       })
   );
 };
