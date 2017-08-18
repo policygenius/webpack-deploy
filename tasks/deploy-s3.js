ab7a4616940d8154f7c7e010e13d03fcfcb28387
Patrick Kim <patrick.kim@policygenius.com>
2017-08-17 22:43:40 -0400

    manifest.json + index.html are no-cached on deploy-s3

--- tasks/deploy-s3.js
+++ tasks/deploy-s3.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const aws = require('gulp-awspublish');
 const gutil = require('gulp-util');
+const tap = require('gulp-tap')
 
 const { getConfigFor } = require('./utils.js');
 
@@ -22,9 +23,19 @@ function deployS3(config) {
   const publisher = aws.create(config.credentials);
 
   // define custom headers
-  const headers = {
-    'Cache-Control': 'max-age=315360000, no-transform, public',
-  };
+  function headers(fileName) {
+    const cacheSetting = 'max-age=315360000, no-transform, public';
+
+    if (fileName === 'manifest.json' || fileName === 'index.html') {
+      cacheSetting = 'no-cache';
+    }
+
+    gutil.log("cache-control:", fileName, cacheSetting);
+
+    return {
+      'Cache-Control': cacheSetting,
+    };
+  }
 
   return (
     gulp
@@ -38,7 +49,11 @@ function deployS3(config) {
       .pipe(aws.gzip())
       // publisher will add Content-Length, Content-Type and headers specified above
       // If not specified it will set x-amz-acl to public-read by default
-      .pipe(publisher.publish(headers))
+      .pipe(
+        tap(function(file) {
+          publisher.publish(headers(file.basename))
+        }),
+      )
       // create a cache file to speed up consecutive uploads
       .pipe(publisher.cache())
       // print upload updates to console
